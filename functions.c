1d26c5ebc3302f911f3ec1bb0d53b92a429677f3
imilanovic997 <104356424+imilanovic997@users.noreply.github.com>
2023-06-01 17:23:41 +0200

    Add files via upload

--- functions.c
+++ functions.c
@@ -2,28 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "functions.h"
+#include "functions.h"										//10
 
-static int brojIgraca = 0, i, j, br = 0;				
+static int brojIgraca = 0, i, j, br = 0;
 
 int izbornik() {
 
-	int opcija = 0;											// 1
-	static IGRAC* polje = NULL;								// 5
+	int opcija = 0;											//1
+	static IGRAC* polje = NULL;								//5
 
 	printf("Dobrodosli u bazu podataka nogometnog kluba!\nOdabirom jednih od opcija uredite, dodajte ili saznajte vise o Vasem omiljenom nogometnom igracu.\n");
-	printf("Pritiskom na broj odaberite opciju\n");			// 8
+	printf("Pritiskom na broj opcije odaberite opciju.\n");		//8
 	printf("' 1 ' Dodaj igraca\n");
 	printf("' 2 ' Uredi postojeceg igraca\n");
 	printf("' 3 ' Ispis igraca\n");
-	
+	printf("' 4 ' Trazilica igraca\n");
+	printf("' 5 ' Sortiraj igrace po registraciji\n");
 
-	scanf("%d", &opcija);
+
+	scanf("%d", &opcija);									
 	system("cls");
 
 	switch (opcija) {
 	case 1:
-		ucitavanjeIgraca();								// 4
+		ucitavanjeIgraca();									//4
 		if (brojIgraca == 0) {
 			kreiranjeDat();
 		}
@@ -40,6 +42,15 @@ int izbornik() {
 		polje = (IGRAC*)ucitavanjeIgraca();
 		ispisivanje(polje);
 		break;
+	case 4:
+		while (opcija != 90) {
+			opcija = izbornikSrch();
+		}
+		break;
+	case 5:
+		while (opcija != 89)
+			opcija = izbornikSrt();
+		break;
 
 	default:
 		printf("\nOdabrali ste pogresan broj!\nMolim probajte ponovno.\n");
@@ -47,11 +58,71 @@ int izbornik() {
 	return opcija;
 }
 
+izbornikSrch() {												//20
+	int opcija = 0;												//9
+	IGRAC* polje = NULL;
+	polje = (IGRAC*)ucitavanjeIgraca();
+	printf("Pretrazi po:\n");
+	printf("\t' 1 ' Imenu\n");
+	printf("\t' 2 ' Prezimenu\n");
+	printf("\t' 3 ' Poziciji\n");
+	printf("\t' 4 ' Broju registracije\n");
+	printf("\t' 5 ' Natrag\n");
+
+	scanf("%d", &opcija);										//9
+	system("cls");
+
+	switch (opcija) {
+	case 1:
+		SearchIme(polje);
+		break;
+	case 2:
+		SearchPrezime(polje);
+		break;
+	case 3:
+		SearchPozicija(polje);
+		break;
+	case 4:
+		SearchBrReg(polje);
+		break;
+	case 5:
+		return 90;
+	default: printf("Odabrali ste pogresan broj!\nMolim probajte ponovno.\n");
+	}
+	return opcija;
+}
+
+int izbornikSrt() {												//21
+	int opcija = 0;
+	IGRAC* polje = NULL;
+	polje = (IGRAC*)ucitavanjeIgraca();
+	printf("Sortiraj prema:\n");
+	printf("\t' 1 ' Broju registracije uzlazno\n");
+	printf("\t' 2 ' Broju registracije silazno\n");
+	printf("\t' 3 ' Natrag\n");
+
+	scanf("%d", &opcija);
+	system("cls");
+
+	switch (opcija) {
+	case 1:
+		selectionSortBrRegUzl(polje);
+		break;
+	case 2:
+		selectionSortBrRegSil(polje);
+		break;
+	case 3:
+		return 89;
+	default: printf("Odabrali ste pogresan broj!\nMolim probajte ponovno.\n");
+	}
+	return opcija;
+}
+
 void kreiranjeDat() {
 	FILE* fp = NULL;
-	fp = fopen("igraci.bin", "wb");					
+	fp = fopen("igraci.bin", "wb");
 	if (fp == NULL) {
-		perror("Kreiranje");
+		perror("Kreiranje");									//19
 	}
 	fwrite(&brojIgraca, sizeof(int), 1, fp);
 	fclose(fp);
@@ -82,7 +153,7 @@ void dodavanjeIgraca() {
 	printf("Unesite broj registracije igraca: ");
 	scanf("%d", &igraci.brreg);
 	getchar();
-	fseek(fp, sizeof(IGRAC) * brojIgraca, SEEK_CUR);
+	fseek(fp, sizeof(IGRAC) * brojIgraca, SEEK_CUR);			//17
 	fwrite(&igraci, sizeof(IGRAC), 1, fp);
 	rewind(fp);
 	brojIgraca++;
@@ -146,3 +217,118 @@ void ispisivanje(IGRAC* polje) {
 	}
 }
 
+void* SearchIme(IGRAC* polje) {
+	char reqName[25];
+	int br = 0;
+
+	printf("Upisite ime trazenog igraca:\n");
+	getchar();
+	scanf("%24[^\n]", reqName);
+
+	for (i = 0; i < brojIgraca; i++) {
+		if (!strcmp(reqName, (polje + i)->ime)) {
+			printf("\nIme:%s  Prezime:%s  Pozicija:%s  Broj registracije: %d\n", (polje + i)->ime, (polje + i)->prezime, (polje + i)->pozicija, (polje + i)->brreg);
+			br++;
+		}
+	}
+	if (br == 0)
+		printf("\nIgrac pod ovim imenom nije pronaden.\n");
+	return NULL;
+}
+
+void* SearchPrezime(IGRAC* polje) {
+	char reqSurname[25];
+	int br = 0;
+
+	printf("Upisite prezime trazenog igraca:\n");
+	getchar();
+	scanf("%29[^\n]", reqSurname);
+
+	for (i = 0; i < brojIgraca; i++) {
+		if (!strcmp(reqSurname, (polje + i)->prezime)) {
+			printf("\nIme:%s  Prezime:%s  Pozicija:%s  Broj registracije: %d\n", (polje + i)->ime, (polje + i)->prezime, (polje + i)->pozicija, (polje + i)->brreg);
+			br++;
+		}
+	}
+	if (br == 0)
+		printf("\nIgrac pod ovim prezimenom nije pronaden.\n");
+	return NULL;
+}
+
+void* SearchPozicija(IGRAC* polje) {
+	char reqPosition[25];
+	int br = 0;
+
+	printf("Upisite poziciju koju trazite:\n");
+	getchar();
+	scanf("%9[^\n]", reqPosition);
+
+	for (i = 0; i < brojIgraca; i++) {
+		if (!strcmp(reqPosition, (polje + i)->pozicija)) {
+			printf("\nIme:%s  Prezime:%s  Pozicija:%s  Broj registracije: %d\n", (polje + i)->ime, (polje + i)->prezime, (polje + i)->pozicija, (polje + i)->brreg);
+			br++;
+		}
+	}
+	if (br == 0)
+		printf("\nNije unesen niti jedan igrac na ovoj poziciji.\n");
+	return NULL;
+}
+
+void* SearchBrReg(IGRAC* polje) {
+	int reqNo, br = 0;
+
+	printf("Unesite broj registracije koju trazite:\n");
+	scanf("%d", &reqNo);
+
+	for (i = 0; i < brojIgraca; i++) {
+		if (reqNo == (polje + i)->brreg) {
+			printf("\nIme:%s  Prezime:%s  Pozicija:%s  Broj registracije: %d\n", (polje + i)->ime, (polje + i)->prezime, (polje + i)->pozicija, (polje + i)->brreg);
+			br++;
+		}
+	}
+	if (br == 0)
+		printf("\nTrazena registracija nije pronadjena.\n");
+	return NULL;
+}
+
+void subs(IGRAC* veci, IGRAC* manji) {
+	IGRAC temp = { 0 };
+	temp = *veci;
+	*veci = *manji;
+	*manji = temp;
+}
+
+void subs2(IGRAC* manji, IGRAC* veci) {
+	IGRAC temp = { 0 };
+	temp = *manji;
+	*manji = *veci;
+	*veci = temp;
+}
+
+void selectionSortBrRegUzl(IGRAC* polje) {
+	for (int j = 0; j < brojIgraca; j++) {
+		for (int i = 0; i < brojIgraca - 1; i++) {
+			if (polje[i].brreg > polje[i + 1].brreg) {
+				subs2((polje + i), (polje + i + 1));
+			}
+		}
+	}
+	for (i = 0; i < brojIgraca; i++) {
+		printf("\nID: %d Ime:%s  Prezime:%s  Pozicija:%s  Broj registracije: %d\n", (polje + i)->id, (polje + i)->ime, (polje + i)->prezime, (polje + i)->pozicija, (polje + i)->brreg);
+	}
+	printf("\n");
+}
+
+void selectionSortBrRegSil(IGRAC* polje) {
+	for (int j = 0; j < brojIgraca; j++) {
+		for (int i = 0; i < brojIgraca - 1; i++) {
+			if (polje[i].brreg < polje[i + 1].brreg) {
+				subs((polje + i), (polje + i + 1));
+			}
+		}
+	}
+	for (i = 0; i < brojIgraca; i++) {
+		printf("\nID: %d Ime:%s  Prezime:%s  Pozicija:%s  Broj registracije: %d\n", (polje + i)->id, (polje + i)->ime, (polje + i)->prezime, (polje + i)->pozicija, (polje + i)->brreg);
+	}
+	printf("\n");
+}
--- functions.h
+++ functions.h
@@ -1,19 +1,29 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-typedef struct igrac {					// 3
+typedef struct igrac {									//3
 	int id;
 	char ime[25];
 	char prezime[30];
 	char pozicija[10];
 	int brreg;
-}IGRAC;									// 2
+}IGRAC;													//2
 
 int izbornik();
+int izbornikSrch();
+int izbornikSrt();
 void kreiranjeDat();
-void dodavanjeIgraca();					// 6
+void dodavanjeIgraca();									//6
 void azuriranje();
 void* ucitavanjeIgraca();
 void ispisivanje(IGRAC*);
+void* SearchIme(IGRAC*);
+void* SearchPrezime(IGRAC*);
+void* SearchPozicija(IGRAC*);
+void* SearchBrReg(IGRAC*);
+void subs(IGRAC*, IGRAC*);
+void subs2(IGRAC*, IGRAC*);
+void selectionSortBrRegUzl(IGRAC*);
+void selectionSortBrRegSil(IGRAC*);
 
 #endif //FUNCTIONS_H
